9cdf248fc316e5e26f345a58e3a52b07a4a885ce
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 17:07:47 +0200

    Updated setup data.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from distutils.core import setup
 setup(
     name='wsgilib',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo priod de>',
     py_modules=['wsgilib'],
-    license=open('LICENSE').read(),
-    description='A simple (U)WSGI framework')
+    license='GPLv3',
+    description='A simple WSGI framework.')
