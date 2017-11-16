92ad5d969c51feb7053076d3439b90d09dd3cc72
Richard Neumann <r.neumann@homeinfo.de>
2017-11-16 15:09:58 +0100

    Updated copyright notices.

--- wsgilib/common.py
+++ wsgilib/common.py
@@ -1,18 +1,17 @@
-#  wsgilib
-#  Copyright (C) 2017  HOMEINFO - Digitale Informationssysteme GmbH
+#  This file is part of wsgilib.
 #
-#  This program is free software: you can redistribute it and/or modify
+#  wsgilib is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
+#  wsgilib is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#  along with wsgilib.  If not, see <http://www.gnu.org/licenses/>.
 """Simple (U)WSGI framework for web applications."""
 
 from functools import lru_cache
