77602edad3807a78755fcc8161ae6d6ac17510c5
Richard Neumann <r.neumann@homeinfo.de>
2017-11-20 14:17:15 +0100

    Fixed typo in email.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='<r dot neumann at homeinfo period de>',
     packages=['wsgilib'],
     license='GPLv3',
     description='A simple WSGI framework.')
