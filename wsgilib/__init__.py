af9758801fb65320ca59a4e8565356546445b002
Richard Neumann <r.neumann@homeinfo.de>
2017-12-05 13:02:43 +0100

    Refactored wsgilib for Flask.

--- wsgilib/__init__.py
+++ wsgilib/__init__.py
@@ -17,22 +17,25 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-"""An object-oriented WSGI micro framework."""
+"""An object-oriented WSGI micro framework based on Flask."""
 
-from .common import Headers, WsgiResponse, Response, Error, PlainText, OK, \
-    HTML, XML, JSON, Binary, InternalServerError, PostData, RequestHandler, \
-    WsgiApp
-from .exceptions import RouteError, InvalidPlaceholderType, InvalidNodeType, \
-    NodeMismatch, UnconsumedPath, PathMismatch, UnmatchedPath
-from .rest import routed, Route, Router, RestApp, RestHandler
+from functools import lru_cache
+from hashlib import sha256
+from traceback import format_exc
+
+from flask import request, Response as Response_, Flask
+from pyxb import PyXBException
+
+from fancylog import LoggingClass
+from mimeutil import mimetype
+from xmldom import DisabledValidation
+
+from wsgilib.json import strip_json, escape_object, json_dumps, json_loads
 
 __all__ = [
-    'routed',
-    'Headers',
-    'WsgiResponse',
     'Response',
-    'Error',
     'PlainText',
+    'Error',
     'OK',
     'HTML',
     'XML',
@@ -40,16 +43,252 @@ __all__ = [
     'Binary',
     'InternalServerError',
     'PostData',
-    'RequestHandler',
-    'WsgiApp',
-    'RouteError',
-    'InvalidPlaceholderType',
-    'InvalidNodeType',
-    'NodeMismatch',
-    'UnconsumedPath',
-    'PathMismatch',
-    'UnmatchedPath',
-    'Route',
-    'Router',
-    'RestApp',
-    'RestHandler']
+    'Application']
+
+
+class Response(Exception, Response_):
+    """An WSGI error message."""
+
+    def __init__(self, msg='', status=200, content_type='text/plain',
+                 charset='utf-8', encoding=True, headers=None):
+        """Generates an error WSGI response."""
+        if encoding:
+            msg = msg.encode(encoding=charset)
+            content_type = '{}; charset={}'.format(content_type, charset)
+
+        Exception.__init__(self, msg)
+        Response_.__init__(
+            self, response=msg, status=status, headers=headers,
+            mimetype=content_type)
+
+
+class PlainText(Response):
+    """Returns a successful plain text response."""
+
+    def __init__(self, msg=None, status=200, charset='utf-8'):
+        """Returns a plain text success response."""
+        super().__init__(
+            msg=msg, status=status, content_type='text/plain',
+            charset=charset, encoding=True)
+
+
+class Error(PlainText):
+    """An WSGI error message."""
+
+    def __init__(self, msg=None, status=400, charset='utf-8'):
+        """Returns a plain text error response."""
+        if 400 <= status < 600:
+            super().__init__(msg=msg, status=status, charset=charset)
+        else:
+            raise ValueError('Not an error status: {}'.format(status))
+
+
+class OK(PlainText):
+    """Returns a successful plain text response."""
+
+    def __init__(self, msg=None, status=200, charset='utf-8'):
+        """Returns a plain text success response."""
+        if 200 <= status < 300:
+            super().__init__(msg=msg, status=status, charset=charset)
+        else:
+            raise ValueError('Not a success status: {}'.format(status))
+
+
+class HTML(Response):
+    """Returns a successful plain text response."""
+
+    def __init__(self, msg=None, status=200, charset='utf-8'):
+        """Returns a plain text success response."""
+        super().__init__(
+            msg=msg, status=status, content_type='text/html',
+            charset=charset, encoding=True)
+
+
+class XML(Response):
+    """An XML response."""
+
+    def __init__(self, dom, validate=True, status=200, charset='utf-8'):
+        """Sets the dom and inherited responde attributes."""
+        if validate:
+            xml_text = dom.toxml(encoding=charset)
+        else:
+            with DisabledValidation():
+                xml_text = dom.toxml(encoding=charset)
+
+        super().__init__(
+            msg=xml_text, status=status, content_type='application/xml',
+            charset=charset, encoding=None)
+
+
+class JSON(Response):
+    """A JSON response."""
+
+    def __init__(self, dictionary, escape=True, strip=False, status=200,
+                 indent=None):
+        """Initializes raiseable WSGI response with
+        the given dictionary d as JSON response.
+        """
+        if strip:
+            dictionary = strip_json(dictionary)
+
+        if escape:
+            dictionary = escape_object(dictionary)
+
+        super().__init__(
+            msg=json_dumps(dictionary, indent=indent), status=status,
+            content_type='application/json', encoding=True)
+
+
+class Binary(Response):
+    """A binary reply."""
+
+    def __init__(self, data, status=200, etag=None, filename=None):
+        """Initializes raiseable WSGI response
+        with binary data and an optional etag.
+        """
+        super().__init__(
+            msg=data, status=status, content_type=mimetype(data),
+            charset=None, encoding=False)
+        self._filename = None
+        self.etag = etag
+        self.filename = filename
+
+    @property
+    def etags(self):
+        """Returns all e-tags."""
+        for key, value in self.headers:
+            if key == 'ETag':
+                yield value
+
+    @property
+    def etag(self):
+        """Returns the e-tag."""
+        for etag in self.etags:
+            return etag
+
+    @etag.setter
+    def etag(self, etag):
+        """Sets the e-tag."""
+        if etag is not None:
+            if etag is True:
+                try:
+                    etag = sha256(self.response[0]).hexdigest()
+                except IndexError:
+                    raise ValueError('No response available to hash.')
+
+            self.headers.append(('ETag', etag))
+
+    @etag.deleter
+    def etag(self):
+        """Deletes all ETags."""
+        self.headers = [
+            (key, value) for key, value in self.headers if key != 'ETag']
+
+    @property
+    def filenames(self):
+        """Yields all file names."""
+        for key, value in self.headers:
+            if key == 'Content-Disposition':
+                _, filename = value.split('; ')
+                _, filename, _ = filename.split('"')
+                yield filename
+
+    @property
+    def filename(self):
+        """Returns the file name."""
+        for filename in self.filenames:
+            return filename
+
+    @filename.setter
+    def filename(self, filename):
+        """Sets the file name.
+
+        Setting the file name to None will also remove
+        any Content-Disposition header field.
+        """
+        if filename is not None:
+            content_disposition = 'attachment; filename="{}"'.format(filename)
+            self.headers.append(('Content-Disposition', content_disposition))
+
+    @filename.deleter
+    def filename(self):
+        """Deletes all file names."""
+        self.headers = [
+            (key, value) for key, value in self.headers
+            if key != 'Content-Disposition']
+
+
+class InternalServerError(Response):
+    """A code-500 WSGI response."""
+
+    def __init__(self, msg='Internal Server Error.', charset='utf-8'):
+        """Indicates an internal server error
+        CORS is enabled by default.
+        """
+        super().__init__(msg=msg, status=500, charset=charset)
+
+
+class PostData:
+    """Represents POST-ed data."""
+
+    file_too_large = Error('File too large.', status=507)
+    no_data_provided = Error('No data provided.')
+    non_utf8_data = Error('POST-ed data is not UTF-8 text.')
+    non_json_data = Error('Text is not vaid JSON.')
+    no_dom_specified = Error('No DOM specified.')
+    invalid_xml_data = Error('Invalid data for XML DOM.')
+
+    def __init__(self, dom=None):
+        """Sets the WSGI input and optional error handlers."""
+        self.dom = dom
+
+    @property
+    @lru_cache(maxsize=1)
+    def bytes(self):
+        """Reads and returns the POST-ed data."""
+        try:
+            return request.get_data()
+        except MemoryError:
+            raise self.file_too_large from None
+
+    @property
+    @lru_cache(maxsize=1)
+    def text(self):
+        """Returns UTF-8 text."""
+        try:
+            return self.bytes.decode()
+        except UnicodeDecodeError:
+            raise self.non_utf8_data from None
+
+    @property
+    def json(self):
+        """Returns a JSON-ish dictionary."""
+        try:
+            return json_loads(self.text)
+        except ValueError:
+            raise self.non_json_data from None
+
+    @property
+    def xml(self):
+        """Loads XML data into the provided DOM model."""
+        if self.dom is None:
+            raise self.no_dom_specified
+
+        try:
+            return self.dom.CreateFromDocument(self.text)
+        except PyXBException:
+            raise self.invalid_xml_data from None
+
+
+class Application(Flask, LoggingClass):
+    """Extended web application basis."""
+
+    def __init__(self, *args, logger=None, debug=False, **kwargs):
+        """Invokes super constructor and adds exception handlers."""
+        Flask.__init__(self, *args, **kwargs)
+        LoggingClass.__init__(self, logger=logger, debug=debug)
+        self.errorhandler(Response)(lambda response: response)
+
+        if debug:
+            self.errorhandler(Exception)(lambda: InternalServerError(
+                msg=format_exc()))
--- wsgilib/json.py
+++ wsgilib/json.py
@@ -23,93 +23,21 @@ from contextlib import suppress
 from datetime import datetime, date, time
 from html import escape as escape_html
 from json import dumps, loads
-from urllib import parse
 
-from strflib import latin2utf
 from timelib import strpdatetime, strpdate, strptime
 
 
 __all__ = [
     'DATE_TIME_TYPES',
-    'PATH_SEP',
-    'HTTP_STATUS',
     'escape_object',
     'json_encode',
     'json_decode',
     'json_dumps',
     'json_loads',
-    'strip_json',
-    'query_items',
-    'query_to_dict',
-    'pathinfo',
-    'iterpath']
+    'strip_json']
 
 
 DATE_TIME_TYPES = (datetime, date, time)
-PATH_SEP = '/'
-HTTP_STATUS = {
-    100: 'Continue',
-    101: 'Switching Protocols',
-    102: 'Processing',
-    200: 'OK',
-    201: 'Created',
-    202: 'Accepted',
-    203: 'Non-Authoritative Information',
-    204: 'No Content',
-    205: 'Reset Content',
-    206: 'Partial Content',
-    207: 'Multi-Status',
-    208: 'Already Reported',
-    226: 'IM Used',
-    300: 'Multiple Choices',
-    301: 'Moved Permanently',
-    302: 'Found',
-    303: 'See Other',
-    304: 'Not Modified',
-    305: 'Use Proxy',
-    306: 'Switch Proxy',  # Deprecated!
-    307: 'Temporary Redirect',
-    308: 'Permanent Redirect',
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    402: 'Payment Required',
-    403: 'Forbidden',
-    404: 'Not Found',
-    405: 'Method Not Allowed',
-    406: 'Not Acceptable',
-    407: 'Proxy Authentication Required',
-    408: 'Request Time-out',
-    409: 'Conflict',
-    410: 'Gone',
-    411: 'Length Required',
-    412: 'Precondition Failed',
-    413: 'Request Entity Too Large',
-    414: 'Request-URL Too Long',
-    415: 'Unsupported Media Type',
-    416: 'Requested range not satisfiable',
-    417: 'Expectation Failed',
-    420: 'Policy Not Fulfilled',
-    421: 'Misdirected Request',
-    422: 'Unprocessable Entity',
-    423: 'Locked',
-    424: 'Failed Dependency',
-    426: 'Upgrade Required',
-    428: 'Precondition Required',
-    429: 'Too Many Requests',
-    431: 'Request Header Fields Too Large',
-    451: 'Unavailable For Legal Reasons',
-    500: 'Internal Server Error',
-    501: 'Not Implemented',
-    502: 'Bad Gateway',
-    503: 'Service Unavailable',
-    504: 'Gateway Time-out',
-    505: 'HTTP Version not supported',
-    506: 'Variant Also Negotiates',
-    507: 'Insufficient Storage',
-    508: 'Loop Detected',
-    509: 'Bandwidth Limit Exceeded',
-    510: 'Not Extended',
-    511: 'Network Authentication Required'}
 
 
 def escape_object(obj):
@@ -199,54 +127,3 @@ def strip_json(dict_or_list):
         raise ValueError('Object must be dict or list.')
 
     return result
-
-
-def query_items(query_string, unquote=True, parsep='&', valsep='='):
-    """Yields key-value pairs of the query string."""
-
-    for parameter in query_string.split(parsep):
-        # Skip empty parameter data.
-        if parameter:
-            fragments = parameter.split(valsep)
-            key = fragments[0]
-
-            # Skip empty-named parameters.
-            if key:
-                if len(fragments) == 1:
-                    value = True
-                else:
-                    value = valsep.join(fragments[1:])
-
-                    if unquote:
-                        value = parse.unquote(value)
-
-                yield (key, value)
-
-
-def query_to_dict(query_string, unquote=True):
-    """Converts a query string into a dictionary."""
-
-    if query_string:
-        return dict(query_items(query_string), unquote=unquote)
-
-    return {}
-
-
-def pathinfo(environ):
-    """Extracts PATH_INFO from env dict."""
-
-    path_info = environ.get('PATH_INFO')
-
-    if path_info is not None:
-        return latin2utf(path_info)
-
-    return None
-
-
-def iterpath(path):
-    """Splits the path."""
-
-    if path:
-        for item in path.split(PATH_SEP):
-            if item:
-                yield item
