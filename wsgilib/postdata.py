9e12c380ea7e1a330a4f597f202876fa28c06850
Richard Neumann <r.neumann@homeinfo.de>
2018-01-16 17:56:56 +0100

    Removed caching.

--- wsgilib/postdata.py
+++ wsgilib/postdata.py
@@ -19,7 +19,6 @@
 # THE SOFTWARE.
 """Post data handling."""
 
-from functools import lru_cache
 from logging import basicConfig, getLogger
 
 from flask import request
@@ -34,31 +33,6 @@ from wsgilib.json import json_loads
 from wsgilib.responses import Error
 
 __all__ = ['PostData']
-
-
-@lru_cache(maxsize=1)
-def bytes_to_text(bytes_):
-    """Converts bytes to text."""
-
-    return bytes_.decode()
-
-
-@lru_cache(maxsize=1)
-def text_to_json(text):
-    """Converts text into a JSON object."""
-
-    return json_loads(text)
-
-
-def text_to_dom(dom, text):
-    """Converts text into a JSON object."""
-
-    @lru_cache(maxsize=1)
-    def load_dom(xml_text):
-        """Actually loads the XML text into the DOM."""
-        return dom.CreateFromDocument(xml_text)
-
-    return load_dom(text)
 
 
 class BytesParser:
@@ -86,7 +60,7 @@ class BytesParser:
     def text(self):
         """Returns UTF-8 text."""
         try:
-            return bytes_to_text(self.bytes)
+            return self.bytes.decode()
         except UnicodeDecodeError:
             raise self.non_utf8_data
 
@@ -94,14 +68,14 @@ class BytesParser:
     def json(self):
         """Returns a JSON-ish dictionary."""
         try:
-            return text_to_json(self.text)
+            return json_loads(self.text)
         except ValueError:
             raise self.non_json_data
 
     def xml(self, dom):
         """Loads XML data into the provided DOM model."""
         try:
-            return text_to_dom(dom, self.text)
+            return dom.CreateFromDocument(self.text)
         except PyXBException:
             raise self.invalid_xml_data
 
