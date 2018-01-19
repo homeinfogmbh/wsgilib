1046be26f7df38393771469e768f47e264ccc2f4
Richard Neumann <r.neumann@homeinfo.de>
2018-01-19 15:25:25 +0100

    Refactored json library.

--- wsgilib/json.py
+++ wsgilib/json.py
@@ -27,17 +27,7 @@ from json import dumps, loads
 from timelib import strpdatetime, strpdate, strptime
 
 
-__all__ = [
-    'DATE_TIME_TYPES',
-    'escape_object',
-    'json_encode',
-    'json_decode',
-    'json_dumps',
-    'json_loads',
-    'strip_json']
-
-
-DATE_TIME_TYPES = (datetime, date, time)
+__all__ = ['escape_object', 'json_dumps', 'json_loads', 'strip_json']
 
 
 def escape_object(obj):
@@ -57,27 +47,38 @@ def escape_object(obj):
 def json_encode(obj):
     """Encodes the object into a JSON-ish value."""
 
-    if isinstance(obj, DATE_TIME_TYPES):
+    if isinstance(obj, (datetime, date, time)):
         return obj.isoformat()
 
     return obj
+
+
+def parse_datetime_date_or_time(string):
+    """Parses datetime, date or time value of the respective string."""
+
+    with suppress(ValueError):
+        return strpdatetime(string)
+
+    with suppress(ValueError):
+        return strpdate(string)
+
+    with suppress(ValueError):
+        return strptime(string)
+
+    raise ValueError('Not a datetime, date or time string.')
 
 
 def json_decode(dictionary):
     """Decodes the JSON-ish dictionary values."""
 
     for key, value in dictionary.items():
-        with suppress(TypeError, ValueError):
-            dictionary[key] = strpdatetime(value)
-            continue
-
-        with suppress(TypeError, ValueError):
-            dictionary[key] = strpdate(value)
-            continue
-
-        with suppress(TypeError, ValueError):
-            dictionary[key] = strptime(value)
-            continue
+        if isinstance(value, str):
+            try:
+                dictionary[key] = parse_datetime_date_or_time(value)
+            except ValueError:
+                pass
+            else:
+                continue
 
     return dictionary
 
@@ -94,36 +95,48 @@ def json_loads(string, *, object_hook=json_decode, **kwargs):
     return loads(string, object_hook=object_hook, **kwargs)
 
 
-def strip_json(dict_or_list):
-    """Strips empty data from JSON-ish objects."""
+def strip_dict(dictionary):
+    """Strips the respective dictionary."""
 
-    if isinstance(dict_or_list, dict):
-        result = {}
+    result = {}
 
-        for key in dict_or_list:
-            value = dict_or_list[key]
+    for key, value in dictionary.items():
+        if isinstance(value, (dict, list)):
+            stripped = strip_json(value)
 
-            if isinstance(value, (dict, list)):
-                stripped = strip_json(value)
-
-                if stripped:
-                    result[key] = stripped
-            elif value is None:
-                continue
-            else:
-                result[key] = value
-    elif isinstance(dict_or_list, list):
-        result = []
-
-        for element in dict_or_list:
-            if isinstance(element, (dict, list)):
-                stripped = strip_json(element)
-
-                if stripped:
-                    result.append(stripped)
-            else:
-                result.append(element)
-    else:
-        raise ValueError('Object must be dict or list.')
+            if stripped:
+                result[key] = stripped
+        elif value is None:
+            continue
+        else:
+            result[key] = value
 
     return result
+
+
+def strip_list(lst):
+    """Strips the respective list."""
+
+    result = []
+
+    for element in lst:
+        if isinstance(element, (dict, list)):
+            stripped = strip_json(element)
+
+            if stripped:
+                result.append(stripped)
+        else:
+            result.append(element)
+
+    return result
+
+
+def strip_json(obj):
+    """Strips empty data from JSON-ish objects."""
+
+    if isinstance(obj, dict):
+        return strip_dict(obj)
+    elif isinstance(obj, list):
+        return strip_list(obj)
+
+    raise ValueError('Object must be dict or list.')
