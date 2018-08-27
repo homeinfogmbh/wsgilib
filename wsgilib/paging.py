418ace3c2fea7f62f36b53c9fd63712e6f833471
Richard Neumann <r.neumann@homeinfo.de>
2018-08-27 14:45:27 +0200

    Implemented paging.

---
+++ wsgilib/paging.py
@@ -0,0 +1,44 @@
+# Copyright 2017 HOMEINFO - Digitale Informationssysteme GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Paging of iterables."""
+
+from flask import request
+
+
+__all__ = ['browse']
+
+
+def browse(iterable, *, page_arg='page', size_arg='size', default_page=0,
+           default_size=10):
+    """Pages the respective iterable."""
+
+    page = int(request.args.get(page_arg, default_page))
+    size = int(request.args.get(size_arg, default_size))
+    first = page * size
+    last = first + size - 1
+
+    for index, item in enumerate(iterable):
+        while index < first:
+            continue
+
+        if index > last:
+            break
+
+        yield item
