c19b7a72acece6df223c07a8f18de1649997001f
Richard Neumann <r.neumann@homeinfo.de>
2018-11-06 15:47:19 +0100

    fix

--- wsgilib/request.py
+++ wsgilib/request.py
@@ -26,8 +26,8 @@ def _dict_from_csv(string):
     """Returns a dict from comma separated values."""
 
     return {
-        key: value for key, value in _split_quality(item)
-        for item in string.split(',') if item}
+        string: quality for sting, quality in (
+            _split_quality(item) for item in string.split(',') if item)}
 
 
 def get_accept():
