8e229193027a7ddeb3edc9c84bb62323ad1218dc
Richard Neumann <r.neumann@homeinfo.de>
2019-01-23 10:32:37 +0100

    Refac.

--- wsgilib/messages.py
+++ wsgilib/messages.py
@@ -3,6 +3,7 @@
 from functools import lru_cache
 from gettext import translation
 from operator import itemgetter
+from pathlib import Path
 
 from wsgilib.request import LANGUAGES
 from wsgilib.responses import JSON
@@ -20,19 +21,49 @@ class LanguageNotFound(Exception):
         self.lang = lang
 
 
-@lru_cache()
-def get_locales(basedir, domain, default=None):
-    """Returns the fist best locale."""
+class MessageFacility:
+    """Manages a message base directory and domain."""
 
-    languages = {} if default is None else dict(default)
-    languages.update(dict(LANGUAGES))
-    languages = sorted(languages.items(), key=itemgetter(1), reverse=True)
-    languages = [language for language, _ in languages]
+    def __init__(self, basedir):
+        """Sets the base directory."""
+        self.basedir = Path(basedir)
 
-    try:
-        return translation(domain, basedir, languages)
-    except FileNotFoundError:
-        raise LanguageNotFound(languages)
+    def domain(self, name):
+        """Returns a message domain."""
+        return Domain(self, name)
+
+
+class Domain:
+    """Manages a message domain."""
+
+    def __init__(self, facility, name, default=None):
+        """Sets facility and name."""
+        self.facility = facility
+        self.name = name
+        self.default = default
+
+    @property
+    def _basedir(self):
+        """Returns the basedir as string."""
+        return str(self.facility.basedir)
+
+    @property
+    @lru_cache()
+    def locales(self):
+        """Returns the fist best locale."""
+        languages = dict(self.default) if self.default else {}
+        languages.update(dict(LANGUAGES))
+        languages = sorted(languages.items(), key=itemgetter(1), reverse=True)
+        languages = [language for language, _ in languages]
+
+        try:
+            return translation(self.name, self._basedir, languages)
+        except FileNotFoundError:
+            raise LanguageNotFound(languages)
+
+    def message(self, msgid, status=200):
+        """Returns a new message for this domain."""
+        return Message(self, msgid, status=status)
 
 
 class Message(Exception):
@@ -40,14 +71,11 @@ class Message(Exception):
     or raised by a web application.
     """
 
-    BASEDIR = NotImplemented
-    DOMAIN = NotImplemented
-    DEFAULT = None
-
-    def __init__(self, msgid, status=200, *,
+    def __init__(self, domain, msgid, status=200, *,
                  localized_message=None, fields=None):
         """Sets message ID and status."""
-        super().__init__(msgid)
+        super().__init__()
+        self.domain = domain
         self.msgid = msgid
         self.status = status
         self._localized_message = localized_message
@@ -63,16 +91,10 @@ class Message(Exception):
             type(self), repr(self.msgid), repr(self.status))
 
     @property
-    def locales(self):
-        """Returns the respective locales."""
-        cls = type(self)
-        return get_locales(cls.BASEDIR, cls.DOMAIN, default=cls.DEFAULT)
-
-    @property
     def localized_message(self):
         """Returns a message with currently set locales."""
         if self._localized_message is None:
-            return self.locales.gettext(self.msgid)
+            return self.domain.locales.gettext(self.msgid)
 
         return self._localized_message
 
@@ -92,7 +114,7 @@ class Message(Exception):
         """Formats the message."""
         localized_message = self.localized_message.format(*args, **kwargs)
         return type(self)(
-            self.msgid, status=self.status,
+            self.domain, self.msgid, status=self.status,
             localized_message=localized_message, fields=self.fields)
 
     def update(self, *dicts, status=None, **kwargs):
@@ -105,5 +127,5 @@ class Message(Exception):
         status = self.status if status is None else status
         fields.update(kwargs)
         return type(self)(
-            self.msgid, status=status,
+            self.domain, self.msgid, status=status,
             localized_message=self._localized_message, fields=fields)
