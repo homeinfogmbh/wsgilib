86a41d058501aabf715208f0eb46a288e658462e
Richard Neumann <mail@richard-neumann.de>
2020-12-23 15:20:24 +0100

    Migrated to setuptools and included dependencies.

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #! /usr/bin/env python3
+"""Installation script"""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='wsgilib',
-    version='latest',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
+    install_requires=['flask', 'werkzeug', 'mimeutil'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo period de>',
+    maintainer_email='<r.neumann@homeinfo.de>',
     packages=['wsgilib'],
     license='GPLv3',
-    description='A simple WSGI framework.'
+    description='W WSGI framework extending flask.'
 )
