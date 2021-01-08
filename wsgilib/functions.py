1c6b94dc4bad8a543dd8e54d8873bfb33bf5eb5d
Richard Neumann <r.neumann@homeinfo.de>
2021-01-08 15:31:39 +0100

    Added default value.

--- wsgilib/functions.py
+++ wsgilib/functions.py
@@ -8,10 +8,14 @@ from flask import request
 __all__ = ['get_bool']
 
 
-def get_bool(key: str, *, source: dict = request.args) -> bool:
+def get_bool(key: str, default: bool = False, *,
+             source: dict = request.args) -> bool:
     """Returns a boolean from the request args or another given source."""
 
-    value = source[key]
+    try:
+        value = source[key]
+    except KeyError:
+        return default
 
     try:
         return RawConfigParser.BOOLEAN_STATES[value.lower()]
