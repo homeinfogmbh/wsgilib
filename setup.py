3d4aaa01ce6c1572b1a82133ab2f88e7f0ba5381
Richard Neumann <mail@richard-neumann.de>
2021-11-29 20:54:50 +0100

    Use setuptools_scm

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 setup(
     name='wsgilib',
-    version_format='{tag}',
-    setup_requires=['setuptools-git-version'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=['flask', 'werkzeug', 'mimeutil'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info@homeinfo.de>',
