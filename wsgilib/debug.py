fe68b623ecf46f18f1b55540c6d4cfb707fbd238
Richard Neumann <r.neumann@homeinfo.de>
2022-11-30 11:01:22 +0100

    Update indentation

--- wsgilib/debug.py
+++ wsgilib/debug.py
@@ -12,9 +12,13 @@ __all__ = ['dump_stacktrace']
 def dump_stacktrace() -> JSONMessage:
     """Dumps a stacktrace of an unexpected exception."""
 
-    print('############################ cut here ############################',
-          file=stderr)
+    print(
+        '############################# cut here #############################',
+        file=stderr
+    )
     print_exc(file=stderr)
-    print('######################## end of traceback ########################',
-          file=stderr, flush=True)
+    print(
+        '######################### end of traceback #########################',
+        file=stderr, flush=True
+    )
     return JSONMessage('Internal server error.', status=500)
